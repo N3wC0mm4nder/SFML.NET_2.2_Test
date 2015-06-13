302a68f9633450e144728628c8b4efac1483ddd4
BuildTools <unconfigured@null.spigotmc.org>
2015-06-13 10:38:30 +0200

    j

---
+++ SFML.NET_2.2_Test/Utilities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFML.NET_2._2_Test
+{
+    class Utilities
+    {
+        public float getFPS(DateTime start, DateTime end)
+        {
+
+        }
+    }
+}
